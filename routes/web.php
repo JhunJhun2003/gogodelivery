8cc2e91874baa07633175f9d758a83c3bc0a613f
JhunJhun2003 <kokyaw3482@gamil.com>
2026-08-26 00:21:23 +0630

    fix the entry route view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->file(resource_path('views/auth/login.html'));
+});
+
+Route::get('/login', function () {
+    return response()->file(resource_path('views/auth/login.html'));
+});
+
+Route::get('/css/{file}', function (string $file) {
+    abort_unless(in_array($file, ['global.css', 'components.css', 'screens.css'], true), 404);
+
+    return response()->file(resource_path("views/css/{$file}"));
+});
+
+Route::get('/js/{file}', function (string $file) {
+    abort_unless(in_array($file, ['sidebar.js', 'history-controls.js'], true), 404);
+
+    return response()->file(resource_path("views/js/{$file}"));
 });
